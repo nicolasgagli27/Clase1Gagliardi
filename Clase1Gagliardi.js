6cd4ef2d49ef4c129912747b2a483e3a69d2d2b8
nicolasgagli27 <nicolasgagli@yahoo.com.ar>
2021-10-29 09:20:17 -0300

    Clase1

---
+++ Clase1Gagliardi.js
@@ -0,0 +1,7 @@
+let nombreUsuario = prompt ("Ingrese su usuario");
+let contraUsuario = prompt ("Ingrese su contraseña");
+
+console.log (nombreUsuario);
+console.log (contraUsuario);
+
+alert ("Información ingresada correctamente");
